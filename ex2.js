7fbc5dc3d8feb7d16145476ed5b52d0bb2100b1f
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-06-26 06:08:33 -0700

    first time using terminal

---
+++ ex2.js
@@ -0,0 +1,8 @@
+/*jshint -W097 */
+/*jshint esversion:6 */
+/*jshint -W033 */
+"use strict";
+
+let username="kiran"
+
+export const getname = () => username
---
+++ example.js
@@ -0,0 +1,82 @@
+/*jshint devel: true */
+/*jshint globalstrict: true*/
+/*jshint esversion: 6*/
+/*jshint -W033 */
+/*jshint -W117 */
+
+"use strict";
+
+// function MyFunc() {
+//   this.name = "kiran";
+//   this.getName = function () {
+//     return this.name;
+//   };
+// }
+
+// MyFunc.prototype.age = 30;
+// MyFunc.prototype.getAge = function () {
+//   return MyFunc.prototype.age;
+// };
+
+// console.log(new MyFunc().getAge());
+
+// class MyClass {
+//   constructor() {
+//     this.name = "kiran";
+//     this.age = 20;
+//   }
+//   getName() {
+//     return this.name;
+//   }
+// }
+// MyClass.prototype.class = "A";
+// MyClass.prototype.getClass = function () {
+//   return MyClass.prototype.class;
+// };
+// console.log(new MyClass().getClass());
+
+// var arr = [8, 7, 6, 1, 2, 3, 4];
+// arr.shift();
+// console.log(arr);
+
+// var func = x => console.log("inside function" + x);
+// let movie = "xmen";
+// console.log(`kiran is ${movie} goood person`); // template literals
+// function myfn(no) {
+//   switch (no) {
+//     case 1:
+//       console.log("one");
+//       break;
+//     case 2:
+//       console.log("two");
+//       break;
+//     default:
+//   }
+// }
+// myfn(2);
+
+
+
+
+
+
+
+
+
+
+// console.log("1");
+// setTimeout(() => {
+//   console.log("2");
+// }, 2000);
+// console.log("3");
+
+
+import { getname } from "./ex2"
+console.log(getname)
+
+
+
+
+
+
+
